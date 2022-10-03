4caf95d9ab4edba0ca513b8c3a78e4cfbad47931
Sean McGeachie <sean@seans-air.home>
2022-10-04 00:11:41 +0100

    initial real commit, from now one we create PRs

---
+++ cypress/component/Home.cy.ts
@@ -0,0 +1,11 @@
+describe('Home.cy.ts', () => {
+  it('playground', () => {
+    // cy.mount()
+  })
+})
+
+describe('InitialTest', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  })
+})
---
+++ cypress/component/index.cy.ts
@@ -0,0 +1,12 @@
+describe('index.cy.ts', () => {
+  it('playground', () => {
+    // cy.mount()
+  })
+})
+
+describe('InitialTest', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  })
+})
+
